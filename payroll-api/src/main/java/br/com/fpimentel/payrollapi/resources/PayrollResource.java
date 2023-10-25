47a11f6b8d9749421e7d8ec3b78cc84679fab684
FulvioFPimentel <fulviofp@hotmail.com>
2023-10-25 10:06:41 -0300

    add feignClients

--- payroll-api/src/main/java/br/com/fpimentel/payrollapi/resources/PayrollResource.java
+++ payroll-api/src/main/java/br/com/fpimentel/payrollapi/resources/PayrollResource.java
@@ -1,5 +1,6 @@
 package br.com.fpimentel.payrollapi.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,19 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fpimentel.payrollapi.domain.Payroll;
+import br.com.fpimentel.payrollapi.domain.User;
+import br.com.fpimentel.payrollapi.feignClients.UserFeign;
 
 @RestController
 @RequestMapping(value = "/api/payments")
 public class PayrollResource {
+	
+	@Autowired
+	private UserFeign userFeign;
 
 	@GetMapping(value = "/{workerId}")
 	public ResponseEntity<Payroll> getPayments(@PathVariable Long workerId, @RequestBody Payroll payment) {
+		
+		User user = userFeign.findById(workerId).getBody();
+		
 		return ResponseEntity
 				.ok()
-				.body(new Payroll("Fulvio", payment.getDescription(),
-						payment.getHourlyPrice(),
-						100.0,
-						payment.getHourlyPrice() * payment.getWorkerHours()));
+				.body(new Payroll(user.getName(), payment.getDescription(),
+						user.getHourlyPrice(),
+						payment.getWorkerHours(),
+						user.getHourlyPrice() * payment.getWorkerHours()));
 	}
 	
 }
